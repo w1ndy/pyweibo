683b19c6a9b4b34170c111351e28d7bb0818c972
Di Weng <mystery.wd@gmail.com>
2016-08-05 13:39:28 +0800

    wrap login example

--- examples/login.py
+++ examples/login.py
@@ -2,15 +2,21 @@ import config
 
 from pyweibo import SinaWeibo
 
-c = config.ExampleConfig('../config.json')
+def doInteractiveLogin():
+    c = config.ExampleConfig('../config.json')
 
-w = SinaWeibo()
-w.getSession().setThrottle(c.http['request_interval'])
-w.getSession().setRequestTimeout(c.http['timeout'])
-w.getSession().setMaxRetries(c.http['max_retries'])
+    w = SinaWeibo()
+    w.getSession().setThrottle(c.http['request_interval'])
+    w.getSession().setRequestTimeout(c.http['timeout'])
+    w.getSession().setMaxRetries(c.http['max_retries'])
 
-captcha_img = w.login(c.cred['username'], c.cred['password'])
-if captcha_img:
-    captcha_img.show()
-    captcha = input('Captcha Code: ')
-    w.login(c.cred['username'], c.cred['password'], captcha)
+    captcha_img = w.login(c.cred['username'], c.cred['password'])
+    if captcha_img:
+        captcha_img.show()
+        captcha = input('Captcha Code: ')
+        w.login(c.cred['username'], c.cred['password'], captcha)
+
+    return w, c
+
+if __name__ == '__main__':
+    doInteractiveLogin()
